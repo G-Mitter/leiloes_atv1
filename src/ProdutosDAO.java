3fece5abea5e5701a5ce49163c4bf04376c46d36
G-Mitter <guimotamitter@gmail.com>
2024-08-31 19:56:03 -0300

    Implementação da funcionalidade da funcionalidade do botão salvar/cadastrar

--- src/ProdutosDAO.java
+++ src/ProdutosDAO.java
@@ -10,8 +10,9 @@
 
 import java.sql.PreparedStatement;
 import java.sql.Connection;
-import javax.swing.JOptionPane;
+
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 
 
@@ -22,9 +23,41 @@ public class ProdutosDAO {
     ResultSet resultset;
     ArrayList<ProdutosDTO> listagem = new ArrayList<>();
     
-    public void cadastrarProduto (ProdutosDTO produto){
+    
+    public void cadastrarProduto (ProdutosDTO p){
         
+          
         
+        conectaDAO conexaoInsert = new conectaDAO();
+        conexaoInsert.conexao();
+
+        if (conexaoInsert.conexao()) {
+
+            String nome = p.getNome();
+            Integer valor  = p.getValor();
+            String status = p.getStatus();
+            
+
+            try {
+
+                String sql = "INSERT INTO produtos (nome, valor, status) VALUES (?, ?, ?)";
+                PreparedStatement stmt = conexaoInsert.getConn().prepareStatement(sql);
+                stmt.setString(1, nome);
+                stmt.setInt(2, valor);
+                stmt.setString(3, status);
+                
+                stmt.executeUpdate();
+                conexaoInsert.desconectar();
+               
+
+            } catch (SQLException sqle) {
+                System.out.println("Erro inserindo : " + sqle.getMessage());
+               
+            }
+        } else {
+           
+        }
+ 
         //conn = new conectaDAO().connectDB();
         
         
--- src/conectaDAO.java
+++ src/conectaDAO.java
@@ -2,6 +2,7 @@
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.sql.Statement;
 import javax.swing.JOptionPane;
 
 
@@ -16,7 +17,7 @@ import javax.swing.JOptionPane;
  * @author Adm
  */
 public class conectaDAO {
-    
+   /* 
     public Connection connectDB(){
         Connection conn = null;
         
@@ -28,6 +29,90 @@ public class conectaDAO {
             JOptionPane.showMessageDialog(null, "Erro ConectaDAO" + erro.getMessage());
         }
         return conn;
+    }
+    */
+    
+    
+    Connection conn; //criando um objeto do tipo connection chamado conn
+    Statement st; //criando um objeto do tipo Statement  chamado st para execução de comando SQL
+
+    public boolean conexao() {
+        try {
+
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            System.out.println("Driver JDBC carregado");
+        } catch (ClassNotFoundException cnfe) {
+            System.out.println("Driver JDBC nao encontrado : "
+                    + cnfe.getMessage());
+        }
+
+        try {
+
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            conn = DriverManager.getConnection("jdbc:mysql://localhost/uc11?user=root&password=1234.Cinco6/7");
+            st = conn.createStatement();
+
+        } catch (ClassNotFoundException ex) {
+            System.out.println("driver não está disponível para acesso ou não existe");
+            return false;
+
+        } catch (SQLException ex) {
+            System.out.println("Sintaxe de comando invalida ");
+            return false;
+        }
+
+        try {
+            st = conn.createStatement();
+            System.out.println("Pronto para execucao de comandos sql.");
+            return true;
+        } catch (SQLException sqle) {
+            System.out.println("Erro no acesso ao Bando de Dados : "
+                    + sqle.getMessage());
+            return false;
+        }
+
+    }
+
+    public conectaDAO() {
+    }
+
+
+    
+    public void desconectar() {
+        try {
+            if (st != null) {
+                st.close();
+            }
+            if (conn != null) {
+                conn.close();
+                System.out.println("Conexão fechada com sucesso.");
+            }
+        } catch (SQLException e) {
+            System.out.println("Erro ao fechar a conexão: " + e.getMessage());
+        }
+    }
+    
+    
+
+    public conectaDAO(Connection conn, Statement st) {
+        this.conn = conn;
+        this.st = st;
+    }
+
+    public Connection getConn() {
+        return conn;
+    }
+
+    public void setConn(Connection conn) {
+        this.conn = conn;
+    }
+
+    public Statement getSt() {
+        return st;
+    }
+
+    public void setSt(Statement st) {
+        this.st = st;
     }
     
 }
